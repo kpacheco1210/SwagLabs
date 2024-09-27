f6a7a48c06fb67d21baaa3c4ff95232db6125303
kpacheco1210 <kpacheco@frba.utn.edu.ar>
2024-09-27 13:24:04 -0300

    add background

---
+++ src/test/java/seleniumgluecode/Background.java
@@ -0,0 +1,26 @@
+package seleniumgluecode;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+public class Background extends TestBase {
+
+  @Given("the user navigates to the login page")
+  public void theUserNavigatesToTheLoginPage() {
+    driver.get("https://www.saucedemo.com/v1/");
+
+    Assert.assertEquals("Swag Labs", driver.getTitle());
+    Assert.assertEquals("https://www.saucedemo.com/v1/", driver.getCurrentUrl());
+  }
+
+  @And("the login page is displayed displayed properly")
+  public void theLoginPageShouldBeDisplayedProperly() {
+    Assert.assertTrue(loginPage.isLoginLogoDisplayed());
+    Assert.assertTrue(loginPage.isUsernameInputDisplayed());
+    Assert.assertTrue(loginPage.isPasswordInputDisplayed());
+    Assert.assertTrue(loginPage.isLoginButtonDisplayed());
+
+  }
+}
